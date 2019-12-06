dca119089d44cdd95e1da92cb7f73cf1425a2450
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-12-06 17:53:26 +0300

    Revert "docs(readme): Write readme"
    This reverts commit 375671e987af984fa5e6e8a021acf57111eeb185.

--- @types/walletconnect/web3-provider.d.ts
+++ @types/walletconnect/web3-provider.d.ts
@@ -2,24 +2,10 @@
 declare module '@walletconnect/web3-provider' {
   import { Provider } from 'web3/providers';
 
-  type ChainId = 1 | 3 | 4 | 5 | 42;
-
   export interface ProviderOptions {
-    /** default: 'https://bridge.walletconnect.org' */
-    bridge?: string;
-    /** default: 4000 */
-    pollingInterval?: number;
-    rpc?: Record<ChainId, string>;
-    infuraId: string;
-    /** default: 1
-     *
-     * 1: 'mainnet'
-     * 3: 'ropsten'
-     * 4: 'rinkeby'
-     * 5: 'goerli'
-     * 42: 'kovan'
-     */
-    chainId?: ChainId;
+    bridge: string;
+    rpc: {};
+    chainId?: 1 | 3 | 4 | 5 | 42;
   }
 
   type Account = string;
