e457b237a6063a158893d3de820a545a9176bcf5
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-01-07 17:35:34 +0300

    fix: fix "can't resolve @web3-wallets-kit/for-third-library-definitions"

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,7 +1,8 @@
 import './@types';
-import { Provider } from '@web3-wallets-kit/for-third-library-definitions';
 
-export * from '@web3-wallets-kit/for-third-library-definitions';
+// TODO rewrite to Type-Only export with typescript@3.8
+// https://github.com/microsoft/TypeScript/pull/35200
+export type Provider = import('@web3-wallets-kit/for-third-library-definitions').Provider;
 
 type MaybePromise<T> = T | Promise<T>;
 
