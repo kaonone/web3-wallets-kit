c7b23dec00c747dc8c7801f0f4a997a1ab7597d4
MorganaLF <4743866@mail.ru>
2020-10-28 16:59:55 +0300

    fix(send function): fix account connecting in new web3 sending interface

--- packages/types/src/@types/for-third-library-definitions.ts
+++ packages/types/src/@types/for-third-library-definitions.ts
@@ -12,22 +12,4 @@ declare module '@web3-wallets-kit/for-third-library-definitions' {
     result?: any;
     error?: string;
   }
-
-  interface Send extends SendOld {
-    // EIP 1193 https://eips.ethereum.org/EIPS/eip-1193
-    (method: string, args?: any[]): Promise<any>;
-  }
-
-  interface SendOld {
-    // Old Web3.js interface
-    (
-      payload: JsonRpcPayload,
-      callback: (error: Error | null, result?: JsonRpcResponse) => void,
-    ): void;
-  }
-
-  export class Provider {
-    send: Send;
-    sendAsync?: SendOld;
-  }
 }
