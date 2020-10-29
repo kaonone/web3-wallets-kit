29d342c82afd0a2868e2a0a434224112ad2ccb52
MorganaLF <4743866@mail.ru>
2020-10-29 08:58:17 +0300

    fix: add missing imported type Provider

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,9 +1,13 @@
 import './@types';
 
+import type { Provider } from '@web3-wallets-kit/for-third-library-definitions';
+
+export { Provider };
+
 type MaybePromise<T> = T | Promise<T>;
 
 export interface DefaultConnectionPayload {
-  provider: any;
+  provider: Provider;
 }
 
 export interface Connector<P extends DefaultConnectionPayload = DefaultConnectionPayload> {
